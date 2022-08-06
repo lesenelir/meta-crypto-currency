96416bce38845736d11bb541575f15f9e3a89cab
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-06 20:51:53 +0800

    feat: first complete navbar

--- client-system/src/components/Navbar/Navbar.jsx
+++ client-system/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,23 @@
+import './Navbar.css'
+
+const NavbarItem = (props) => {
+  return (
+      <li>{props.title}</li>
+  )
+}
+
+const navbarArr = ["Market", "Exchange", "Tutorials", "Wallets"]
+
 function Navbar() {
   return (
-      <h1>Navbar</h1>
+      <div className="navbar-box">
+        <ul>
+          {navbarArr.map((item, index) => (
+              <NavbarItem key={index} title={item}/>
+          ))}
+          <li className="login-li">Login</li>
+        </ul>
+      </div>
   )
 }
 
