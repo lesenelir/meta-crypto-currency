427e1c5ce7f147cc00e5e06be39f81079836ef54
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-07 01:29:59 +0800

    feat: Welcome   border-radiu: 10px; Left

---
+++ client-system/src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import './Loader.css'
+
+function Loader() {
+  return (
+      <div className="loader-box">
+        Loading...
+      </div>
+  )
+}
+
+export default Loader
--- client-system/src/components/Welcome/Welcome.jsx
+++ client-system/src/components/Welcome/Welcome.jsx
@@ -1,12 +1,55 @@
 
-
 import './Welcome.css'
 
 
+const Input = () => {
+
+}
+
+
 function Welcome() {
+
+  const connectWallet = () => {
+
+  }
+
   return (
       <div className="welcome-box">
-      1
+        <div className="welcome-box-left">
+          <div className="box-left-top">
+           <h1 className="welcome-h1">Send Crypto <br/> around the world </h1>
+           <p className="welcome-p">
+             Explore the web3.0 world.
+             Buy and sell crypto currencies easily.
+             <br/>
+             Lesenelir's Crypto App to be your crypto market partner.
+           </p>
+          </div>
+          <button
+             className="connect-btn"
+             onClick={connectWallet}
+          >
+           <p>Connect Wallet</p>
+          </button>
+          <div className="box-left-bottom">
+            <div className="l">
+              <div className="l1">Reliability</div>
+              <div className="l2">Web3.0</div>
+            </div>
+            <div className="m">
+              <div className="m1">Security</div>
+              <div className="m2">Ethereum</div>
+            </div>
+            <div className="r">
+              <div className="r1">Low fees</div>
+              <div className="r2">Blockchain</div>
+            </div>
+          </div>
+        </div>
+
+       <div className="welcome-box-right">
+         Right
+       </div>
       </div>
   )
 }
