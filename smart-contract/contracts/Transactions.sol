e9bf2a7ffb6e9f6a0fb89851ef318f1818765ba6
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-07 23:08:04 +0800

    complete smart contract

--- smart-contract/contracts/Transactions.sol
+++ smart-contract/contracts/Transactions.sol
@@ -3,4 +3,41 @@ pragma solidity ^0.8.0;
 
 contract Transactions {
 
+    uint256 transactionCount;
+
+    event Transfer(address from, address receiver, uint amout, string message, uint256 timestamp, string keyword);
+
+    // transaction needs to have
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp; // when to send transfer
+        string keyword;
+    }
+
+    // TransferStruct Array
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        // add transactions
+        transactionCount += 1;
+        // push a specific transactions into Transactions Array
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+         return transactions;
+    }
+
+    function getTransactionsCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
 }
+
+// Metamask injects the Ethereum Web3 API into every website's js context
+// so that DApps can read from the blockchain
--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle')
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const {privateKey} = require('./secrets/secrets.json')
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/SfHtz1_uP5sVwQfKspMI9fiJ-K1V7nFc',
+      accounts: [privateKey]
+    }
+  }
 };
