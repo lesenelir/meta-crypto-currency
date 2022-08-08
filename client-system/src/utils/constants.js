ae0bbe6ce00625254626141b72a9a6417f0e27be
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-08 15:47:36 +0800

    feat: add utils

---
+++ client-system/src/utils/constants.js
@@ -0,0 +1,8 @@
+import abi from './Transactions.json'
+
+
+// 合约地址 由 以下命令生成
+// npx hardhat run scripts/deploy.js --network rinkeby
+export const contractAddress = '0xA1a9eAE1dBf15C25f52aa4DcA0b3a55FC9d3371c'
+
+export const contractABI = abi.abi
