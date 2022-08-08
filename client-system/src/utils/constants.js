8119cbc7271caaff0ea9a6b08f4c530cfc57e2fd
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-08 18:11:34 +0800

    setup utils information with smart contract

--- client-system/src/utils/constants.js
+++ client-system/src/utils/constants.js
@@ -3,6 +3,6 @@ import abi from './Transactions.json'
 
 // 合约地址 由 以下命令生成
 // npx hardhat run scripts/deploy.js --network rinkeby
-export const contractAddress = '0xA1a9eAE1dBf15C25f52aa4DcA0b3a55FC9d3371c'
+export const contractAddress = '0x21c1DaCE57Ec66516515311C22Dfce09a2E2B8Ad'
 
 export const contractABI = abi.abi
