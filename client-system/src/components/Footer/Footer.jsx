ca9a91d55c7e7b8f6f8b0de78be414bc9736face
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-09 23:31:37 +0800

    feat: finish Footer components

--- client-system/src/components/Footer/Footer.jsx
+++ client-system/src/components/Footer/Footer.jsx
@@ -1,6 +1,20 @@
+import './Footer.css'
+
 function Footer() {
   return (
-      <h1>Footer</h1>
+      <div className="footer-box">
+        <div className="footer-text">
+          <p>Come join us and hear for the unexpected miracle</p>
+          <p>miaomiaobabyzy@gmail.com</p>
+        </div>
+
+        <div className="footer-hr" />
+
+        <div className="footer-bottom">
+          <p>@LesenelirCrypto2022</p>
+          <p>All rights reserved</p>
+        </div>
+      </div>
   )
 }
 
