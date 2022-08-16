1774d4c5f082a939943d72de86448da9c4b1dc11
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-16 13:56:10 +0800

    update

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,12 +1,14 @@
 // responsible for deploying the contract
 
 const main = async () => {
+
+  // 生成ContractFactory
   const Transactions = await hre.ethers.getContractFactory("Transactions")
-  const transactions = await Transactions.deploy()
+  const transactions = await Transactions.deploy() // 合约实例
 
-  await transactions.deployed()
+  await transactions.deployed() // 合约实例部署
 
-  console.log("Transactions deployed to : ", transactions.address)
+  console.log("Transactions deployed to : ", transactions.address) // 打印合约的地址
 }
 
 
