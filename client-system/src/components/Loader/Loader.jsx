27720d815513c9a586e1a5438076a430b91480ef
lesenelir <miaomiaobabyzy@gmail.com>
2022-11-11 21:45:19 +0800

    chore: change code style

--- client-system/src/components/Loader/Loader.jsx
+++ client-system/src/components/Loader/Loader.jsx
@@ -2,11 +2,11 @@ import './Loader.css'
 
 function Loader() {
   return (
-      <div className="spinner">
-        <div className="bounce1"/>
-        <div className="bounce2"/>
-        <div className="bounce3"/>
-      </div>
+    <div className="spinner">
+      <div className="bounce1"/>
+      <div className="bounce2"/>
+      <div className="bounce3"/>
+    </div>
   )
 }
 
