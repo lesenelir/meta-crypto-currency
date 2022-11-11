01faa1943e121d85dde7a2ca9a112f1041540ec7
lesenelir <miaomiaobabyzy@gmail.com>
2022-11-11 21:41:56 +0800

    chore: navbar

--- client-system/src/components/Navbar/Navbar.jsx
+++ client-system/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 
 const NavbarItem = (props) => {
   return (
-      <li>{props.title}</li>
+    <li>{props.title}</li>
   )
 }
 
@@ -12,18 +12,18 @@ const navbarArr = ["Market", "Exchange", "Tutorials", "Wallets"]
 
 function Navbar() {
   return (
-      <div className="navbar-box">
-        <div className="img-box">
-          <img src={logo} alt="" width={50} height={50} />
-          <h2>Lesenelir's Crypto Website</h2>
-        </div>
-        <ul>
-          {navbarArr.map((item, index) => (
-              <NavbarItem key={item + index} title={item}/>
-          ))}
-          <li className="login-li">Login</li>
-        </ul>
+    <div className="navbar-box">
+      <div className="img-box">
+        <img src={logo} alt="" width={50} height={50}/>
+        <h2>Lesenelir's Crypto Website</h2>
       </div>
+      <ul>
+        {navbarArr.map((item, index) => (
+          <NavbarItem key={item + index} title={item}/>
+        ))}
+        <li className="login-li">Login</li>
+      </ul>
+    </div>
   )
 }
 
