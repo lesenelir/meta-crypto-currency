9b37f5b8fee1e175ef418c1a20784c73d0feb776
lesenelir <miaomiaobabyzy@gmail.com>
2022-11-11 21:40:25 +0800

    chore: transaction

--- client-system/src/components/Transactions/Transactions.jsx
+++ client-system/src/components/Transactions/Transactions.jsx
@@ -15,67 +15,67 @@ const TransactionCard = (props) => {
   const gifUrl = useFetch({keyword})
 
   return (
-      <div className="transaction-card-box">
-        {/* 文字 */}
-        <div className="transaction-card-text-box">
-          <a href={`https://rinkeby.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
-            <p>From: {shortenAddress(addressFrom)}</p>
-          </a>
-          <a href={`https://rinkeby.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
-            <p>To: {shortenAddress(addressTo)}</p>
-          </a>
-          <p>Amount: {amount} ETH</p>
-          {
-            message ? (
-                <p>Message: {message}</p>
-            ) : (
-                <p>Message: no message</p>
-            )
-          }
-        </div>
-        {/* 图片 */}
-        <img
-            src={gifUrl || "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284" }
-            alt="gif"
-            className="transaction-card-img"
-        />
-
-        {/* 时间戳 */}
-        <div className="transaction-card-time-box">
-          <p>{timestamp}</p>
-        </div>
-
+    <div className="transaction-card-box">
+      {/* 文字 */}
+      <div className="transaction-card-text-box">
+        <a href={`https://rinkeby.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+          <p>From: {shortenAddress(addressFrom)}</p>
+        </a>
+        <a href={`https://rinkeby.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+          <p>To: {shortenAddress(addressTo)}</p>
+        </a>
+        <p>Amount: {amount} ETH</p>
+        {
+          message ? (
+            <p>Message: {message}</p>
+          ) : (
+            <p>Message: no message</p>
+          )
+        }
       </div>
+      {/* 图片 */}
+      <img
+        src={gifUrl || "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"}
+        alt="gif"
+        className="transaction-card-img"
+      />
+
+      {/* 时间戳 */}
+      <div className="transaction-card-time-box">
+        <p>{timestamp}</p>
+      </div>
+
+    </div>
   )
 }
 
 
 function Transactions() {
 
-  const { currentAccount, transactions } = useContext(TransactionContext)
+  const {currentAccount, transactions} = useContext(TransactionContext)
 
   // 渲染交易列表
   return (
-      <div className="transaction-box">
-        <div className="transaction-box-top">
-          {
-            currentAccount ? (
-                <h3>Latest Transactions</h3>
-            ) : (
-                <h3>Connect your account to see the latest transactions</h3>
-            )
-          }
-        </div>
-
-        <div className="transaction-box-bottom">
-          {
-            // 截取数组的最后8个作为显示
-            transactions.slice(-8).map((item, index) => (
-              <TransactionCard key={index} {...item} />
-            ))
-          }
-        </div>
+    <div className="transaction-box">
+      <div className="transaction-box-top">
+        {
+          currentAccount ? (
+            <h3>Latest Transactions</h3>
+          ) : (
+            <h3>Connect your account to see the latest transactions</h3>
+          )
+        }
       </div>
+
+      <div className="transaction-box-bottom">
+        {
+          // 截取数组的最后8个作为显示
+          transactions.slice(-8).map((item, index) => (
+            <TransactionCard key={index} {...item} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
