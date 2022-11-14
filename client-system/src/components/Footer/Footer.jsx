ff3bd88318d51b0efaca76d8e2742966519d06fb
lesenelir <miaomiaobabyzy@gmail.com>
2022-11-14 21:20:09 +0800

    chore: code styles

--- client-system/src/components/Footer/Footer.jsx
+++ client-system/src/components/Footer/Footer.jsx
@@ -2,19 +2,19 @@ import './Footer.css'
 
 function Footer() {
   return (
-      <div className="footer-box">
-        <div className="footer-text">
-          <p>Come to join us and witness for the unexpected miracle</p>
-          <p>miaomiaobabyzy@gmail.com</p>
-        </div>
-
-        <div className="footer-hr" />
-
-        <div className="footer-bottom">
-          <p>@LesenelirCrypto2022</p>
-          <p>All rights reserved</p>
-        </div>
+    <div className="footer-box">
+      <div className="footer-text">
+        <p>Come to join us and witness for the unexpected miracle</p>
+        <p>miaomiaobabyzy@gmail.com</p>
       </div>
+
+      <div className="footer-hr"/>
+
+      <div className="footer-bottom">
+        <p>@LesenelirCrypto2022</p>
+        <p>All rights reserved</p>
+      </div>
+    </div>
   )
 }
 
